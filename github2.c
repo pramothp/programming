6d9b4b0c7ca8f88a1822dda6142d63c4bb353664
pramothp <pramoth74@gmail.com>
2016-12-28 09:33:24 +0530

    Add files via upload

---
+++ github2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int n;
+    printf("\n ener the number:");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+
+        printf("\n the given number is even");
+
+    }
+    else
+
+    {
+
+        printf("\n the given number is odd");
+
+    }
+}
