a40b77d4e2ad5f7aa96281c168ab60afadafaea0
pramothp <pramoth74@gmail.com>
2016-12-28 09:22:00 +0530

    Add files via upload

---
+++ github1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int n,i;
+printf("\n enter the number :");
+scanf("%d",&n);
+
+if(n>0)
+    {
+        printf("\n the given number is positive");
+
+    }
+ if(n<0)
+ {
+     printf("\n  the given number is negative" );
+
+ }
+ if(n==0)
+ {
+     printf("\n the number is zero");
+ }
+}
