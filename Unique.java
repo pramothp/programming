ecf568290c5713df879609461f2a579832888d8f
pramothp <pramoth74@gmail.com>
2017-06-28 10:10:15 +0530

    Add files via upload

---
+++ Unique.java
@@ -0,0 +1,30 @@
+package hunter;
+import java.util.Scanner;
+public class Unique {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner (System.in);
+		int n=s.nextInt();
+		int count=0;
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			count=0;
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]==a[j])
+				{
+					count++;
+				}
+				
+				}
+			if(count==1){
+				System.out.println(" "+a[i]);
+			}
+		}
+s.close();
+	}
+
+}
