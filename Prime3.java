e5cca3b7b9eb60e46338eb915f032a3d8ba3ba14
pramothp <pramoth74@gmail.com>
2017-07-05 09:47:57 +0530

    Add files via upload

--- Prime3.java
+++ Prime3.java
@@ -9,19 +9,23 @@ public class Prime3 {
 		int count=0;
 		int sum=0;
 		for(int i=n;i>=2;i--){
-		for(int j=2;j<=i;j++){
 			m=n;
 			count=0;
-			if(n%j==0)
+		for(int j=2;j<=i;j++){
+			
+			if(i%j==0)
 			{
 				count++;
 			}
 		}
+		System.out.println(count);
 		n--;
 		
 		
     if(count==1){
+    	System.out.println(m);
     	if(m%10==3){
+    		System.out.println(sum);
     	sum=sum+m;
     }
 	}
