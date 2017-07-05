6ae15620b116121fa788674bd3efb8236ccb62ab
pramothp <pramoth74@gmail.com>
2017-07-05 11:44:06 +0530

    Add files via upload

--- Sum.java
+++ Sum.java
@@ -1,32 +1,34 @@
 package hunter;
-
 import java.util.Scanner;
-
 public class Sum {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		int x[]=new int [n];
-		int sum1=99999999;
-		int sum;
-		int a=0;int b=0;
-		for(int  i=0;i<n;i++){
-			x[i]=s.nextInt();
-		}
-		for(int i=0;i<n;i++)
-		{
-			for(int j=i+1;j<n;j++)
-			{
-			sum=Math.abs(x[i]+x[j]);
-			if(sum<sum1){
-				a=x[i];
-				b=x[j];
-				sum1=sum;
-			}
+	Scanner s=new Scanner(System.in);
+	int m=s.nextInt();
+	int count=0;
+	int n=m;
+	int rem1=0;
+	int sum=0;
+	while(m!=0)
+	{
+		 rem1=m%10;
+		m=m/10;
+		count++;
 	}
-		}
-		System.out.println("the two elements are "+a+" & " +b);
-		s.close();
-		}
+	rem1=0;
+	for(int i=0;i<count;i++){
+		int b=n;
+	
+	while(b!=0)
+	{
+		 rem1=b%10;
+		b=b/10;
+		sum=sum+rem1;
+	}
+	 n=n/10;
+	}
+System.out.println(sum);
+s.close();
+	}
+
 }
